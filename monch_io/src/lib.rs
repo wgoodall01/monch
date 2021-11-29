07c17746eff4cc550359b95fa710d6422624d800
William Goodall <wgoodall01@gmail.com>
2021-11-28 19:41:46 -0500

    Implement basic 'monch_util_ls'

--- monch_io/src/lib.rs
+++ monch_io/src/lib.rs
@@ -1,8 +1,65 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::{Deserialize, Serialize};
+use std::io;
+use thiserror::Error;
+
+/// Re-export the `cbor!` macro to implement our `put!` macro
+pub use ciborium::cbor;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    Io(#[from] io::Error),
+
+    #[error("constructing output value: {0}")]
+    ConstructValue(#[from] ciborium::value::Error),
+
+    #[error("writing object: {0}")]
+    Serialize(#[from] ciborium::ser::Error<io::Error>),
+
+    #[error("reading object: {0}")]
+    Deserialize(#[from] ciborium::de::Error<io::Error>),
+}
+
+/// Logs a message for humans to standard error.
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt) *) => { eprintln!($($arg) *) }
+}
+
+/// Writes an object for machines to standard out.
+#[macro_export]
+macro_rules! try_put {
+    // Take invocations like `put!({"key" => "value"})` or `put!(["a", "list""])`
+    ({ $($toks:tt) * }) => { ::monch_io::try_put!(@ cbor { $($toks) * }) };
+    ([ $($toks:tt) * ]) => { ::monch_io::try_put!(@ cbor [ $($toks) * ]) };
+
+    (@ cbor $($toks:tt) *) => {{
+        use monch_io::{cbor, Error, try_put};
+
+        let result: Result<(), Error> = cbor!($($toks) *)
+            .map_err(|e| Error::ConstructValue(e))
+            .and_then(|val| try_put!(&val));
+
+        result
+    }};
+
+    ($value:expr) => { ::monch_io::write($value) };
+}
+
+/// Writes an object for machines to standard out. If the write fails, panic.
+#[macro_export]
+macro_rules! put {
+    ( $($toks:tt) * ) => { ::monch_io::try_put!( $($toks) * ).expect("failed to put object") };
+}
+
+/// Write a serializable object to structured stdout.
+pub fn write<T: Serialize>(object: &T) -> Result<(), Error> {
+    ciborium::ser::into_writer(object, io::stdout())?;
+    Ok(())
+}
+
+/// Read a deserializable object from structured stdin.
+pub fn read<'a, T: Deserialize<'a>>() -> Result<T, Error> {
+    let obj = ciborium::de::from_reader(io::stdin())?;
+    Ok(obj)
 }
--- monch_util_ls/src/main.rs
+++ monch_util_ls/src/main.rs
@@ -1,3 +1,92 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Error};
+use clap::Parser;
+use monch_io::{log, put};
+use serde::Serialize;
+use std::{env, fs, path::PathBuf};
+
+#[derive(Debug, Parser)]
+struct Args {
+    directory: Option<PathBuf>,
+
+    /// Do not ignore directory entries starting with `.`
+    #[clap(short('a'), long)]
+    all: bool,
+
+    /// Include file metadata
+    #[clap(short('l'), long)]
+    long: bool,
+}
+
+#[derive(Serialize)]
+struct DirEntry {
+    name: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    kind: Option<String>,
+}
+
+fn main() -> Result<(), Error> {
+    let args = Args::parse();
+
+    // Determine the directory we're reading from
+    let dir = match args.directory {
+        Some(dir) => dir,
+        None => env::current_dir().context("no working directory")?,
+    };
+
+    // Read the directory
+    let entries = fs::read_dir(dir).context("failed to read directory")?;
+
+    // Iterate over the directory entries
+    for entry_result in entries {
+        let entry = entry_result.context("failed to read directory entry")?;
+
+        // Get the filename, and check that it's valid Unicode.
+        let filename_os = entry.file_name();
+        let name: String = match filename_os.into_string() {
+            Ok(s) => s,
+
+            // If the filename wasn't valid Unicode, log a warning, and skip to the next file.
+            Err(original) => {
+                log!(
+                    "ls: encountered file with non-utf8 name: {}",
+                    original.to_string_lossy()
+                );
+                continue; // skip to the next file
+            }
+        };
+
+        // Depending on the `-a` flag, skip hidden entries (starting with a dot)
+        if !args.all && name.starts_with('.') {
+            continue; // skip this file
+        }
+
+        // Depending on if `-l` was passed, write extended information or just the filename to
+        // stdout.
+        if args.long {
+            // Write extended information about the file, in an object like `{name, kind, ..}`.
+
+            let meta = entry.metadata().context("failed to read file metadata")?;
+
+            // Based on file metadata, come up with a type
+            let kind = if meta.is_dir() {
+                "Dir"
+            } else if meta.is_file() {
+                "File"
+            } else {
+                "Unknown"
+            };
+
+            // Output the filename and extended information.
+            put!({
+                "name" => name,
+                "kind" => kind,
+            });
+        } else {
+            // Output the filename and nothing else.
+            put!(&name);
+        }
+    }
+
+    Ok(())
 }
